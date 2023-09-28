4663aa733ec32497b69bad85e9edfe9b6dad21f0
jerrymusaga <musagatechnology@gmail.com>
2023-09-28 14:22:29 +0100

    setting up .env and rpc endpoint for swisstronik

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
+      accounts: [PRIVATE_KEY || ""], //Your private key starting with "0x"
+      //Make sure you have enough funds in this wallet to deploy the smart contract
+    },
+  },
 };
 
 export default config;
